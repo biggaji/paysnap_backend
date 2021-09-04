b0a0a2006c25403b4e3be1ad1b1a7dd12e1c4d31
Tobi Ajibade <tobiajibade2017@yandex.com>
2021-09-04 13:26:01 +0100

    Added the update password method to settings class

--- src/dataSources/settings.ts
+++ src/dataSources/settings.ts
@@ -8,7 +8,35 @@ class Setting {
 
   // updateUsername(newusername: string, id: string) {}
 
-  async updatePassword(opts: updatePasswordOpts, id:string) {}
+  async updatePassword(opts: updatePasswordOpts, id:string) {
+
+    let { oldpassword, newpassword } = opts;
+    let updatedPassword;
+
+    try {
+
+      // fetch current password
+
+      let cPassword:any = await db.query(`SELECT password FROM users WHERE id = $1`, [id]);
+
+      cPassword = cPassword.rows[0].password;
+       
+      // compare current pin with the old password
+
+      let comparePasswords = await compare(oldpassword, cPassword);
+
+      if(!comparePasswords) {
+        return new UserInputError("The old password you entered is incorrect");
+      } else {
+        let hashedPassword = await hash(newpassword, 10);
+        updatedPassword = await db.query(`UPDATE users SET password = $1 WHERE id = $2 RETURNING password`, [hashedPassword, id]);
+        return (updatedPassword.rows[0].password !== null) ? true : false;
+      }
+
+    } catch (error) {
+      throw error;
+    }
+  }
 
   async updatePin(opts: updatePinOpts, id:string) {
     let oPin:any, nPin:any;
@@ -24,10 +52,10 @@ class Setting {
 
       currentPin = currentPin.rows[0].pin;
 
-      let compareOldPin = await compare(oPin, currentPin);
+      let comparePins = await compare(oPin, currentPin);
 
-      if(!compareOldPin) {
-        return new UserInputError("The old pin is incorrect");
+      if(!comparePins) {
+        return new UserInputError("The old pin you entered is incorrect");
       } else {
         let hashedPin = await hash(nPin, 10);
         pinUpdate = await db.query(
@@ -36,7 +64,7 @@ class Setting {
         );
       }
 
-      return pinUpdate.rows[0].pin !== null ? true : false;
+      return (pinUpdate.rows[0].pin !== null) ? true : false;
     } catch (error) {
       throw error;
     }
